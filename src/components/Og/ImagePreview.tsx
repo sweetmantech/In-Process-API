9d6c1cd604cfb27d193012e330e0fc5c1ece2c14
k.c <tech.eng.me@gmail.com>
2026-02-20 05:55:11 +1000

    Revert "Revert "Merge pull request #41 from sweetmantech/techengme/myc-4293-artist-og-image-broken-for-webp-moments""
    This reverts commit 25535167a7adec8ba3ac2244a45ddec5952e0a31.

--- src/components/Og/ImagePreview.tsx
+++ src/components/Og/ImagePreview.tsx
@@ -1,10 +1,12 @@
 import { ARTIST_OG_IMAGE_WIDTH, rotation } from '@/lib/og/consts';
 import { ImageMetadata } from '@/types/og';
 
-const TokenImagePreview = ({
+const ImagePreview = ({
   imageMetadata,
+  containerWidth = ARTIST_OG_IMAGE_WIDTH,
 }: {
   imageMetadata: ImageMetadata | null;
+  containerWidth?: number;
 }) => {
   if (!imageMetadata)
     return (
@@ -32,8 +34,7 @@ const TokenImagePreview = ({
   } = imageMetadata;
   const paddingLeft =
     (Math.abs(
-      (ARTIST_OG_IMAGE_WIDTH / originalHeight) * originalWidth -
-        ARTIST_OG_IMAGE_WIDTH
+      (containerWidth / originalHeight) * originalWidth - containerWidth
     ) /
       2) *
     -1;
@@ -44,7 +45,7 @@ const TokenImagePreview = ({
   if (originalWidth > originalHeight) style.height = '100%';
   else
     style.width = shouldRotate
-      ? (ARTIST_OG_IMAGE_WIDTH / originalHeight) * originalWidth
+      ? (containerWidth / originalHeight) * originalWidth
       : '100%';
   return (
     // eslint-disable-next-line
@@ -52,4 +53,4 @@ const TokenImagePreview = ({
   );
 };
 
-export default TokenImagePreview;
+export default ImagePreview;
