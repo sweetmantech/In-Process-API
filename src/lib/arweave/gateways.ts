14e9b16d4355064f3881255ad07f8f2fde40efb5
ziad <tech.eng.me@gmail.com>
2026-03-03 12:35:22 +1000

    safe gateway

--- src/lib/arweave/gateways.ts
+++ src/lib/arweave/gateways.ts
@@ -1,8 +1,5 @@
 const gateways = [
-  'https://turbo-gateway.com',
-  'https://arweave.ar',
-  'https://permagate.io',
-  'https://ardrive.net',
+  'https://turbo-gateway.com'
 ];
 
 export default gateways;
