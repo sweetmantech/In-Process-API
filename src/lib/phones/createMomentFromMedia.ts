006024781a91493bb40ac31513b7ba01f033dbe4
ziad <tech.eng.me@gmail.com>
2026-03-26 00:48:56 +1000

    fix: build error

--- src/lib/phones/createMomentFromMedia.ts
+++ src/lib/phones/createMomentFromMedia.ts
@@ -30,7 +30,7 @@ const createMomentFromMedia = async (
       payoutRecipient: artistAddress as Address,
     },
     account: artistAddress as Address,
-    channel: 'sms',
+    channel: 'sms' as const,
   };
   const { contractAddress, tokenId } = await createMoment(
     momentCreateParameters
