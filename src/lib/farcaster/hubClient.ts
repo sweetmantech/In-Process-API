1baa83589e688f1ed45142453fce12ae36ad4fc2
ziad <tech.eng.me@gmail.com>
2026-04-10 01:33:45 +1000

    fix: custody address

--- src/lib/farcaster/hubClient.ts
+++ src/lib/farcaster/hubClient.ts
@@ -1,6 +1,6 @@
-import { getSSLHubRpcClient } from '@farcaster/hub-nodejs';
+import { getInsecureHubRpcClient } from '@farcaster/hub-nodejs';
 import { FARCASTER_HUB_GRPC } from '@/lib/consts';
 
-const hubClient = getSSLHubRpcClient(FARCASTER_HUB_GRPC);
+const hubClient = getInsecureHubRpcClient(FARCASTER_HUB_GRPC);
 
 export default hubClient;
