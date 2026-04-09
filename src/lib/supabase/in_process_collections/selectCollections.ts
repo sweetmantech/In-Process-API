3ef56ff59bd44cbcf0481d17f521ea515ab1ed7c
ziad <tech.eng.me@gmail.com>
2026-04-10 09:47:16 +1000

    remove in case of page is undefined

--- src/lib/supabase/in_process_collections/selectCollections.ts
+++ src/lib/supabase/in_process_collections/selectCollections.ts
@@ -38,8 +38,6 @@ const selectCollections = async ({
   if (page !== undefined) {
     query = query.in('admins.token_id', [0]);
     query = query.range((page - 1) * cappedLimit, page * cappedLimit - 1);
-  } else {
-    query = query.limit(cappedLimit);
   }
   query = query.order('created_at', { ascending: false });
 
