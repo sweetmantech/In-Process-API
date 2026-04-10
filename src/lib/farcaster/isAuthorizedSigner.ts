eeb940987c49714decc2451742760a4a9bcefe28
ziad <tech.eng.me@gmail.com>
2026-04-10 22:50:09 +1000

    feat: track artist farcaster username

--- src/lib/farcaster/isAuthorizedSigner.ts
+++ src/lib/farcaster/isAuthorizedSigner.ts
@@ -1,11 +1,16 @@
 import getFarcasterAddresses from '@/lib/farcaster/getFarcasterAddresses';
+import { upsertArtistNames } from '@/lib/supabase/in_process_artists/upsertArtistNames';
 
 const isAuthorizedSigner = async (
   fid: bigint,
   signer: string
 ): Promise<{ authorized: boolean; verifiedAddress: string }> => {
-  const { custodyAddress, verifiedAddress } = await getFarcasterAddresses(fid);
+  const { custodyAddress, verifiedAddress, artistName } =
+    await getFarcasterAddresses(fid);
   const authorized = signer.toLowerCase() === custodyAddress;
+  if (authorized && artistName) {
+    await upsertArtistNames(new Map([[verifiedAddress, artistName]]));
+  }
   return { authorized, verifiedAddress };
 };
 
