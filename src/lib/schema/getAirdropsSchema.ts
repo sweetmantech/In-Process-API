85e299f54974214babe22f179f4ab84b43a9d952
ziad <tech.eng.me@gmail.com>
2026-04-14 02:59:49 +1000

    Remove: legacy architecture regarding in_process_airdrops

--- src/lib/schema/getAirdropsSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import addressSchema from './addressSchema';
-import chainIdSchema from './chainIdSchema';
-
-export const getAirdropsSchema = z.object({
-  artist_address: addressSchema,
-  chainId: chainIdSchema,
-  offset: z.coerce.number().optional().default(0),
-});
