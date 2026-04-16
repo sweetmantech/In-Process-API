9b21e1943e4fc64ba7e6654082a0645ddb2ffe69
ziad <tech.eng.me@gmail.com>
2026-04-17 07:55:05 +1000

    indexer name

--- src/lib/indexer/zoraAdminsIndexer.ts
+++ src/lib/indexer/zoraAdminsIndexer.ts
@@ -6,7 +6,7 @@ import type { IndexConfig } from '@/types/indexerFactory';
 export const zoraAdminsIndexer: IndexConfig<ZoraMedia_Admins_t> = {
   processBatchFn: processAdminsInBatches,
   selectMaxTimestampFn: selectMaxGrantedAt,
-  indexName: 'zoraAdmins',
+  indexName: 'zora_admins',
   dataPath: 'ZoraMedia_Admins',
   queryFragment: `ZoraMedia_Admins(limit: $limit, offset: $offset_zoraAdmins, order_by: {updated_at: asc}, where: {updated_at: {_gt: $minTimestamp_zoraAdmins}}) {
     id admin collection token_id chain_id permission updated_at
--- src/lib/indexer/zoraMomentsIndexer.ts
+++ src/lib/indexer/zoraMomentsIndexer.ts
@@ -6,7 +6,7 @@ import type { IndexConfig } from '@/types/indexerFactory';
 export const zoraMomentsIndexer: IndexConfig<ZoraMedia_Moments_t> = {
   processBatchFn: processMomentsInBatches,
   selectMaxTimestampFn: selectMaxUpdatedAt,
-  indexName: 'zoraMoments',
+  indexName: 'zora_moments',
   dataPath: 'ZoraMedia_Moments',
   queryFragment: `ZoraMedia_Moments(limit: $limit, offset: $offset_zoraMoments, order_by: {updated_at: asc}, where: {updated_at: {_gt: $minTimestamp_zoraMoments}}) {
     id collection token_id owner uri metadata_uri chain_id created_at updated_at transaction_hash
