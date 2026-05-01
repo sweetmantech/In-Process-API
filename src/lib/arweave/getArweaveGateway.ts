dfe43384cdfa9fb6b575665dc85dd097b18e7ba7
ziad <tech.eng.me@gmail.com>
2026-05-02 00:10:44 +1000

    remove legacy api

--- src/lib/arweave/getArweaveGateway.ts
+++
@@ -1,9 +0,0 @@
-import wayfinderClient from './wayfinderClient';
-
-const getArweaveGateway = async (): Promise<URL> => {
-  const { strategy } = wayfinderClient.routingSettings;
-  if (!strategy) throw new Error('Wayfinder routing strategy not configured');
-  return strategy.selectGateway({});
-};
-
-export default getArweaveGateway;
