18e98d642bd2833babd2cd3f052aac60362af28e
ziad <tech.eng.me@gmail.com>
2026-05-08 18:20:52 +1000

    remove arweave uploads api

--- src/app/api/arweave/logs/route.ts
+++ src/app/api/arweave/logs/route.ts
@@ -1,6 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import validateLogArweaveUpload from '@/lib/arweave/validateLogArweaveUpload';
-import logArweaveUploadsHandler from '@/lib/arweave/logArweaveUploadsHandler';
 import validateGetArweaveLogsQuery from '@/lib/arweave/validateGetArweaveLogsQuery';
 import getArweaveLogsHandler from '@/lib/arweave/getArweaveLogsHandler';
 
@@ -9,19 +7,6 @@ export async function GET(req: NextRequest) {
     const validated = await validateGetArweaveLogsQuery(req);
     if (validated instanceof NextResponse) return validated;
     return getArweaveLogsHandler(validated);
-  } catch (e: unknown) {
-    const message = e instanceof Error ? e.message : 'Failed';
-    return NextResponse.json({ message }, { status: 500 });
-  }
-}
-
-export async function POST(req: NextRequest) {
-  try {
-    const validated = await validateLogArweaveUpload(req);
-    if (validated instanceof NextResponse) return validated;
-
-    const { artistAddress, uploads } = validated;
-    return logArweaveUploadsHandler(artistAddress, uploads);
   } catch (e: unknown) {
     const message = e instanceof Error ? e.message : 'Failed';
     return NextResponse.json({ message }, { status: 500 });
