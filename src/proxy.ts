740144b288940c5fcdf3b48893b134149c7bb0e2
ziad <tech.eng.me@gmail.com>
2026-05-19 01:42:28 +1000

    fix cors error

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,7 +6,7 @@ export function proxy(request: NextRequest) {
       status: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
         'Access-Control-Allow-Headers':
           'Content-Type, Authorization, x-api-key, x-recaptcha-token',
       },
