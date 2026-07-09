efbeb328b3bbb2f3a2320e4c3a220a146686d19f
ziad <tech.eng.me@gmail.com>
2026-07-09 13:45:38 +1000

    include youtube links

--- src/lib/telegram/chat/commands/handleStart.ts
+++ src/lib/telegram/chat/commands/handleStart.ts
@@ -6,7 +6,7 @@ const handleStart = async (
   artistUsername: string | null,
   telegramUsername: string
 ) => {
-  const text = `Hello ${artistUsername || telegramUsername}, Your telegram has been connected! You can now send photos and captions to post them on In Process.`;
+  const text = `Hello ${artistUsername || telegramUsername}! Your Telegram is connected. You can now send photos, videos, or YouTube links to create moments.`;
   await thread.post(text);
 };
 
